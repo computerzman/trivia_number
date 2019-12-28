e58b2299ce0a9913374a07490f996871eed7a896
Admin <mhe.developer@gmail.com>
2019-12-28 10:49:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'injection_container.dart' as di;
-
+ 
 void main() async {
   // add the following line, and it should be the first line in main method
   WidgetsFlutterBinding.ensureInitialized(); /* To Fix the Error: Flutter: Unhandled Exception: ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized https://stackoverflow.com/questions/57689492/flutter-unhandled-exception-servicesbinding-defaultbinarymessenger-was-accesse */
